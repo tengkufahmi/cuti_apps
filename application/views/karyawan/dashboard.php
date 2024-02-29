6a216dbbdf51c176d1fb92f96c2194c68daf24c7
tengkufahmi <tuapayers@gmail.com>
2024-02-29 09:04:51 +0700

    fixing logic sisa cuti

--- application/views/karyawan/dashboard.php
+++ application/views/karyawan/dashboard.php
@@ -67,7 +67,7 @@
                                 </div>
                                 <a href="<?= base_url(); ?>Cuti/view_karyawan/<?= $this->session->userdata('id_user'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
-                         </div>
+                        </div>
 
                         <div class="col-lg-3 col-6">
                             <div class="small-box bg-success">
@@ -75,7 +75,7 @@
                                     <h3><?= $cuti_acc['total_cuti'] ?></h3>
 
                                     <p>Data Cuti Diterima</p>
-                                    </div>
+                                </div>
                                 <div class="icon">
                                     <i class="ion ion-stats-bars"></i>
                                 </div>
@@ -96,7 +96,7 @@
                                 <a href="<?= base_url(); ?>Cuti/view_karyawan/<?= $this->session->userdata('id_user'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
-                       
+
                         <div class="col-lg-3 col-6">
                             <div class="small-box bg-warning">
                                 <div class="inner">
@@ -115,85 +115,89 @@
                                 <div class="inner">
                                     <h3><?php
 
-                                        if ($cuti_karyawan == null) {
-                                            echo 'Belum Ada';
-                                        } else {
-                                            $now = time();
-                                            $your_date = strtotime($cuti_karyawan[0]['berakhir']);
-                                            $datediff = $your_date - $now;
+                                    if ($cuti_karyawan == null) {
+                                        echo 'Belum Ada';
+                                    } else {
 
-                                            $date_akhir = round($datediff / (60 * 60 * 24));
+                                        $now = time();
+                                        $mulai = strtotime($cuti_karyawan[0]['mulai']);
+                                        $berakhir = strtotime($cuti_karyawan[0]['berakhir']);
 
-                                            $now = time();
-                                            $your_date = strtotime($cuti_karyawan[0]['mulai']);
-                                            $datediff = $now - $your_date;
+                                        $hari_kerja = 0;
 
-                                            $date_mulai = round($datediff / (60 * 60 * 24));
-
-                                            if ($date_mulai >= 0 and $date_akhir >= 0) {
-                                                echo $date_akhir . ' Hari Lagi';
-                                            } else {
-                                                echo "Belum Ada";
+                                        while ($mulai <= $berakhir) {
+                                            /* Cek jika hari ini bukan Sabtu (6) atau Minggu (7) */
+                                            if (date('N', $mulai) < 6) {
+                                                $hari_kerja++;
                                             }
+
+                                            $mulai = strtotime('+1 day', $mulai);
                                         }
 
-                                        ?></h3>
+                                        if ($hari_kerja > 0) {
+                                            echo $hari_kerja . ' Hari Lagi';
+                                        } else {
+                                            echo "Belum Ada";
+                                        }
+                                    }
 
-                                    <p>Sisa Cuti</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="<?= base_url(); ?>Cuti/view_karyawan/<?= $this->session->userdata('id_user'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                ?></h3>
+
+                                <p>Sisa Cuti</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="<?= base_url(); ?>Cuti/view_karyawan/<?= $this->session->userdata('id_user'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <h1>Syarat Permohonan Cuti</h1>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text"> Cuti Bepergian : (Maksimal 3 Hari) </p>
                             </div>
                         </div>
                     </div>
 
-                    <h1>Syarat Permohonan Cuti</h1>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p class="card-text"> Cuti Bepergian : (Maksimal 3 Hari) </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-sm-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p class="card-text"> Cuti Hamil : (Maksimal 4 Bulan) </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p class="card-text"> Cuti Sakit : (Maksimal 7 Hari) </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-sm-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p class="card-text"> Cuti Penting : (Maksimal 1 Bulan) </p>
-                                </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text"> Cuti Hamil : (Maksimal 4 Bulan) </p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
-        </div>
-        
-        <aside class="control-sidebar control-sidebar-dark">
-        </aside>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text"> Cuti Sakit : (Maksimal 7 Hari) </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text"> Cuti Penting : (Maksimal 1 Bulan) </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 
-    <?php $this->load->view("karyawan/components/js.php") ?>
+    <aside class="control-sidebar control-sidebar-dark">
+    </aside>
+</div>
+
+<?php $this->load->view("karyawan/components/js.php") ?>
 </body>
 
 </html>
